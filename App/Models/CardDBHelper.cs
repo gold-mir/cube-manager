81dcdfccefa58ebac3c4592419a8bf353d057778
Miranda <miranda.keith.42@gmail.com>
2018-02-22 15:52:22 -0800

    add database schema

---
+++ App/Models/CardDBHelper.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Application.Models
+{
+    public class CardDBHelper
+    {
+        public static string[] GetAllCardsJson()
+        {
+              
+        }
+
+        public static void RepopulateCards()
+        {
+
+        }
+    }
+}
