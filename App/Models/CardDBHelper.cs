d2b212420c1c8da3f5b465076ad12d94544cbf72
Miranda <miranda.keith.42@gmail.com>
2018-02-27 16:46:02 -0800

    updated

--- App/Models/CardDBHelper.cs
+++ App/Models/CardDBHelper.cs
@@ -7,9 +7,11 @@ namespace Application.Models
 {
     public class CardDBHelper
     {
+        //Special Cases: Who // What // When // Where // Why, Brisela, Hanweir, Chittering Host
         public static string[] GetAllCardsJson()
         {
-              
+            dynamic allCardsJSON;
+
         }
 
         public static void RepopulateCards()
